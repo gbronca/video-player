e7536230ccb2c856e8137fd222d845056f041ae3
gbronca <42703789+gbronca@users.noreply.github.com>
2020-11-03 21:03:47 +0000

    play now updates the progress bar and current play time.

--- script.js
+++ script.js
@@ -31,8 +31,22 @@ function togglePlay() {
 // Show play button icon on video end
 video.addEventListener('ended', showPlayIcon);
 
-
 // ---- Progress Bar ---- //
+
+// Calculate display time format
+function displayTime(time) {
+	const minutes = Math.floor(time / 60);
+	let seconds = Math.floor(time % 60);
+	seconds = seconds > 9 ? seconds : `0${seconds}`;
+	return `${minutes}:${seconds}`;
+}
+
+function updateProgress() {
+	progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+	currentTime.textContent = `${displayTime(video.currentTime)} /`;
+	duration.textContent = `${displayTime(video.duration)}`;
+}
+
 
 
 
@@ -40,3 +54,5 @@ video.addEventListener('ended', showPlayIcon);
 // ---- Event Listeners ---- //
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
